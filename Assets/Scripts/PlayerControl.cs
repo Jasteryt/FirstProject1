50e84958fc5120c15b16d8735fc79df4908e3d2f
Jasteryt <mz6016130@gmail.com>
2026-05-16 17:20:37 +0500

    Сделал логику игрока
    Сделал логику игрока ходьба на a and d

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,6 +1,30 @@
 using UnityEngine;
 
-public class PlayerControl
+public class PlayerControl : MonoBehaviour
 {
+    public float speed = 1;
+    private Vector3 velocity;
+    public float gravity = -9.81f;
+    private Rigidbody2D rb;
+    private float horizontal;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        
+    }
+       
+    
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+       
+
     
 }
